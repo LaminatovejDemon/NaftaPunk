386d765fd9d08410872ea5d4a67ae8b53723898f
Pokorny, Filip <Filip.Pokorny@disney.com>
2014-01-25 21:18:33 +0100

    PK: gunfire

--- Assets/Src/Trooper.cs
+++ Assets/Src/Trooper.cs
@@ -14,6 +14,8 @@ public class Trooper : MonoBehaviour {
 	public float SHOOT_ANGLE_DOT = 0.8f;
 	public string NAME = "Abdul";
 
+	public Transform _GunfireParticle;
+
 	public int _SkillSpeed = 1;
 	int _SkillSpeedLocal = -1;
 
@@ -61,11 +63,7 @@ public class Trooper : MonoBehaviour {
 		targetPos_.y = transform.position.y;
 		float ret_ = Quaternion.LookRotation(transform.position - target.transform.position).eulerAngles.y;                                   
 
-
 		ret_ = (int)((ret_+30.0f)/60.0f) * 60;
-
-		Debug.Log ("Object angle is " + ret_);
-
 
 		return ret_;
 	}
