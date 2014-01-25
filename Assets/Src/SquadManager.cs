da55742e2393130ebb9ad5fcfe574f99480765f7
Miroslav Manena <phoshee@gmail.com>
2014-01-25 19:32:22 +0100

    phoshee: spawners oprava2

--- Assets/Src/SquadManager.cs
+++ Assets/Src/SquadManager.cs
@@ -257,7 +257,7 @@ public class SquadManager : MonoBehaviour
 	{
 		foreach( Trooper t in _AllyList )
 		{
-			if( _Pathfinding.GetTileBelow(t.transform.position) )
+			if( _Pathfinding.GetTileBelow(t.transform.position) == spawner )
 				return true;
 			if( HasVisibility(t, spawner, false) )
 				return true;
