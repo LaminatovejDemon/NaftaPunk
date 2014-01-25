66ee376efa85308afabc779ac8b4fa47cf3bfed0
Pokorny, Filip <Filip.Pokorny@disney.com>
2014-01-25 15:42:06 +0100

    PK: floor tiles final

--- Assets/Src/Level/HexData.cs
+++ Assets/Src/Level/HexData.cs
@@ -4,8 +4,22 @@ using System.Collections;
 public class HexData : MonoBehaviour 
 {
 	public bool m_Spawner;
+	public int m_HexType;
 
 	private Trooper m_SpawnedTrooper = null;
+
+	void Start ()
+	{
+		m_HexType = Random.Range(0,16);
+
+
+		if ( m_HexType == 13 ) m_HexType = 16; // black ceiling 1
+		else if ( m_HexType == 12 ) m_HexType = 10; // black ceiling 2
+		else if ( m_HexType == 8 ) m_HexType = 6; // grail
+		else if ( m_HexType == 4 ) m_HexType = 2; // teleport
+
+		Utils.SetUV(renderer.material, m_HexType);
+	}
 
 	// Update is called once per frame
 	void Update () 
