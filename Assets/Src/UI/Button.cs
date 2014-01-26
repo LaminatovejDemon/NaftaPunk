5049a85ae38c3182bc3d971e9fa30c94b04655b4
Pokorny, Filip <Filip.Pokorny@disney.com>
2014-01-26 02:58:58 +0100

    PK: fraction select screen

--- Assets/Src/UI/Button.cs
+++ Assets/Src/UI/Button.cs
@@ -6,14 +6,17 @@ public class Button : MonoBehaviour
 	public GameObject _Target;
 	public string _Message;
 
+	public Color _SelectedColor = new Color(0.3f,0.3f,0.3f,1);
+	public Color _StandardColor = new Color(0.5f,0.5f,0.5f,1);
+
 	public void OnTouchDown()
 	{
-		renderer.material.color = Color.black;
+		renderer.material.SetColor("_TintColor", _SelectedColor);
 	}
 
 	public void OnTouchUp()
 	{
-		renderer.material.color = Color.white;
+		renderer.material.SetColor("_TintColor", _StandardColor);
 		if ( _Target != null && _Message != null )
 		{
 			_Target.SendMessage(_Message, SendMessageOptions.RequireReceiver);
@@ -22,7 +25,7 @@ public class Button : MonoBehaviour
 
 	public void OnTouchCancel()
 	{
-		renderer.material.color = Color.white;
+		renderer.material.SetColor("_TintColor", _StandardColor);
 	}
 
 }
