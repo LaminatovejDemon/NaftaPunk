25632dc050573a6b72e569ffd4a744aee0b2f247
Pokorny, Filip <Filip.Pokorny@disney.com>
2014-01-26 12:25:19 +0100

    PK: sounds alpha

---
+++ Assets/Src/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MusicManager : MonoBehaviour {
+
+
+	static MusicManager _Instance = null;
+
+	public static MusicManager GetInstance()
+	{
+		return _Instance;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		if ( _Instance != null )
+		{
+			GameObject.Destroy(this);
+			return;
+		}
+		_Instance = this;
+
+		DontDestroyOnLoad(this);
+	}
+
+	public enum Music
+	{
+		Menu,
+		Ingame,
+	};
+
+	public void Play(Music what)
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
